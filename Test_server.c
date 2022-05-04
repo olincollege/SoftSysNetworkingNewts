9509ea237e4a8c3eed3781617f3c164ed5dd6a45
Cbabe <cbabe@olin.edu>
2022-05-04 11:16:08 -0400

    Test_Server

---
+++ Test_server.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/rfcomm.h>
+
+int main(int argc, char **argv)
+{
+    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
+    char buf[1024] = { 0 };
+    int s, client, bytes_read;
+    socklen_t opt = sizeof(rem_addr);
+    char message[100];
+   int status=0;
+    // allocate socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    // bind socket to port 1 of the first available 
+    // local bluetooth adapter
+    loc_addr.rc_family = AF_BLUETOOTH;
+    loc_addr.rc_bdaddr = *BDADDR_ANY;
+    loc_addr.rc_channel = (uint8_t) 1;
+    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+
+    // put socket into listening mode
+    listen(s, 1);
+    
+    char n='y';
+    // accept one connection
+    while(1){
+        client = accept(s, (struct sockaddr *)&rem_addr, &opt);
+        ba2str( &rem_addr.rc_bdaddr, buf );
+        fprintf(stderr, "accepted connection from %s\n", buf);
+        memset(buf, 0, sizeof(buf));
+        printf("Do you want to connect?(y/n) ");
+        char i[2];
+        scanf("%c", &i);
+        if (strcmp(i,n)){
+            while (1) {
+                printf("Vedaant: ");
+                scanf("%s", message);
+                
+                if (write(client, message, sizeof(message)) < 0) {
+                    perror("Uh oh");
+                }
+                bytes_read = read(client, buf, sizeof(buf));
+                if( bytes_read > 0 ) {
+                    printf("Charlie: %s\n", buf);
+                }
+            }
+        }
+        else{
+            close(client);
+        }
+    }
+    // close connection
+    close(s);
+    return 0;
+}
