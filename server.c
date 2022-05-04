48197fe642f3e831cee80bd1ec1ff47ed5fe35b1
VedaantKuchhal <vedaantk22@gmail.com>
2022-05-04 17:15:04 -0400

    Refactor server code

--- server.c
+++ server.c
@@ -1,17 +1,48 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <bluetooth/bluetooth.h>
-#include <bluetooth/rfcomm.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <stdio.h>
+#include "server.h"
 
-void *chat_read(void *my_client)
+int s, client;
+
+/* Set up a signal handler.
+
+   sig: signal number
+   handler: signal handler function
+*/
+int catch_signal(int sig, void (*handler) (int)) {
+    struct sigaction action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    return sigaction(sig, &action, NULL);
+}
+
+/* Signal handler: Close the socket.
+ */
+void close_socket(int sig)
+{
+    close(client);
+    close(s);
+    printf("\nClosing connection!\n");
+    exit(EXIT_SUCCESS);
+}
+
+/* 
+
+Writes the user's output to the socket 
+
+Args: 
+    my_client: A void pointer to an integer storing the client number
+
+*/
+void *chat_write(void *my_client)
 {   
+    /* Max message size is 100 */
     char message[100];
+
+    /* Cast client to int pointer */
     int client;
-    client = (int)my_client;
+    client = (intptr_t) my_client;
+
+    /* Wait for user input, and write it to client. */
     while(1){
         fgets(message, sizeof(message), stdin);  
         if (write(client, message, sizeof(message)) < 0) {
@@ -22,77 +53,108 @@ void *chat_read(void *my_client)
    pthread_exit(NULL);
 }
 
-void *chat_write(void *my_client)
-{
+/* 
+
+Reads the from the socket 
+
+Args: 
+    my_client: A void pointer to an integer storing the client number
+
+*/
+void *chat_read(void *my_client)
+{   
+    /* Maximum message buffer size of 1024 */
     char buf[1024] = { 0 };
+
+    /* Cast client to int pointer */
     int client;
-    client = (int)my_client;
+    client = (intptr_t) my_client;
+
+    /* Repeatedly read from socket */
     int bytes_read;
     while(1){
+        /* Close socket if interrupted */
+        catch_signal(SIGINT, close_socket);
+
         bytes_read = read(client, buf, sizeof(buf));
+        /* If data is detected, print output */
         if( bytes_read > 0 ) {
             printf("Charlie: ");
             printf("%s", buf);
         }
    }
+   /* Exit thread */
    pthread_exit(NULL);
 }
 
-
+/* Main function to establish connection with client */
 int main(int argc, char **argv)
-{
+{   
+    /* Initialize threads */
     pthread_t thread_a;
     pthread_t thread_b;
     int rc;
+    char i;
+
+    /* Initialize socket */
     struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
     char buf[1024] = { 0 };
-    int s, client, bytes_read;
+    int bytes_read;
     socklen_t opt = sizeof(rem_addr);
+
+    /* Set message size to 100 */
     char message[100];
-   int status=0;
-    // allocate socket
+
+    int status = 0;
+    /* Allocate socket */
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    // bind socket to port 1 of the first available 
-    // local bluetooth adapter
+    /* Bind socket to Port 1 of the first available local bluetooth adapter */
     loc_addr.rc_family = AF_BLUETOOTH;
     loc_addr.rc_bdaddr = *BDADDR_ANY;
     loc_addr.rc_channel = (uint8_t) 1;
     bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
 
-    // put socket into listening mode
+    /* Put socket into listening mode. */
     listen(s, 1);
 
-    char y='y';
-    // accept one connection
+    /* Accept one connection */
     client = accept(s, (struct sockaddr *)&rem_addr, &opt);
     ba2str( &rem_addr.rc_bdaddr, buf );
+
     fprintf(stderr, "Connection from %s\n", buf);
     memset(buf, 0, sizeof(buf));
+
+    /* Ask user if they want to connect */
     printf("Do you want to connect?(y/n) ");
-    char i;
     scanf("%c", &i);
-    if (i==y){
-        rc = pthread_create(&thread_a, NULL, chat_read, (void *)client);
+
+    if (i=='y'){
+        /* Handle break and exit program */
+        /* Create threads for reading from and writing to other user */
+        rc = pthread_create(&thread_a, NULL, chat_write, (void *) (intptr_t) client);
         if (rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
         }
-        rc = pthread_create(&thread_b, NULL, chat_write, (void *)client);
+        rc = pthread_create(&thread_b, NULL, chat_read, (void *) (intptr_t) client);
         if (rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
             exit(-1);
-            }
-            pthread_exit(NULL);
-            }
-    else{
+        }
+        pthread_exit(NULL);
+    }
+    else {
+        /* Inform user that they will not be connected to */
         char message[11]="No connect";
         if (write(client, message, sizeof(message)) < 0) {
             perror("Uh oh");
         }
         printf("Closing\n");
+        /* Close connection */
+        close(client);
+        close(s);
+        return 0;
     }
-    close(client);
-    close(s);
     return 0;
 }
--- server.h
+++ server.h
@@ -3,5 +3,12 @@
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
 
-int server();
+void *chat_write(void *my_client);
+void *chat_read(void *my_client);
+
