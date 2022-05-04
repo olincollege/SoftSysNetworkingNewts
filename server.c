33ddf5e0759ce4c0ca474bad93fb585212fc2c9a
Cbabe <cbabe@olin.edu>
2022-05-04 14:43:41 -0400

    Added to server

--- server.c
+++ server.c
@@ -14,16 +14,39 @@ int client;
 pid_t pid; 
 
 // Create widget for email entry.
-    GtkWidget *showEmail; 
+   
 
 GtkWidget *emailLabel, *emailEntry, *signupBtn, *grid;
 
 gchar buffer[10000];
 
+void *chat_write(void *my_client,  GtkLabel *showEmail)
+{   
+    char buf[1024] = { 0 };
+    int bytes_read;
+    while(1){
+        bytes_read = read(client, buf, sizeof(buf));
+        if( bytes_read > 0 ) {
+            printf("Charlie: ");
+            printf("%s", buf);
+
+            // Append previous data to current data
+            // strcpy(buffer, "hello");
+            strcat(buf, "\n");
+            strcat(buffer, buf);
+            strcat(buffer, "\n");
+            g_print("%s\n", buffer);
+
+            // Print out the text sent to data to the label emailData
+            gtk_label_set_text(GTK_LABEL(showEmail), (const gchar *) buffer); 
+        }
+   }
+}
+
 void signup_button_clicked(GtkWidget *wid,gpointer data)
  {  
     // Get the text from the email entry 
-    gchar* emailData = gtk_entry_get_text(GTK_ENTRY(emailEntry)); 
+    const gchar* emailData = gtk_entry_get_text(GTK_ENTRY(emailEntry)); 
 
     // Append previous data to current data
     // strcpy(buffer, "hello");
@@ -32,7 +55,7 @@ void signup_button_clicked(GtkWidget *wid,gpointer data)
     strcat(buffer2, "\n");
     strcat(buffer, emailData);
     strcat(buffer, "\n");
-    g_printf("%s\n", buffer);
+    g_print("%s\n", buffer);
 
     if (write(client, buffer2, sizeof(buffer2)) < 0) {
             perror("Uh oh");
@@ -49,6 +72,7 @@ static void activate (GtkApplication* app, gpointer user_data)
  {  
      // Create a new window
     GtkWidget *window;
+    GtkLabel *showEmail; 
     window = gtk_application_window_new (app);
 
     // Set title and size
@@ -70,8 +94,13 @@ static void activate (GtkApplication* app, gpointer user_data)
 
 
     // Send the value when signupBtn is clicked to call signup_button_clicked
+    pid = fork();
+        if (pid == 0){
+            chat_write(client,showEmail);
+        }
+        else {
     g_signal_connect(signupBtn,"clicked",G_CALLBACK(signup_button_clicked),showEmail);
-
+        }
     // Create a new GTK box
     GtkWidget *box; box = gtk_box_new(GTK_ORIENTATION_VERTICAL,2);
 
@@ -88,47 +117,6 @@ static void activate (GtkApplication* app, gpointer user_data)
     gtk_widget_show_all (window);
  } 
 
-void *chat_read(void *my_client)
-{   
-    char message[100];
-    int client;
-    client = (int)my_client;
-    while(1){
-        fgets(message, sizeof(message), stdin);  
-        if (write(client, message, sizeof(message)) < 0) {
-            perror("Uh oh");
-        }
-
-
-    }
-   pthread_exit(NULL);
-}
-
-void *chat_write(void *my_client)
-{   
-    
-    char buf[1024] = { 0 };
-    int client;
-    client = (int)my_client;
-    int bytes_read;
-    while(1){
-        bytes_read = read(client, buf, sizeof(buf));
-        if( bytes_read > 0 ) {
-            printf("Charlie: ");
-            printf("%s", buf);
-
-            // Append previous data to current data
-            // strcpy(buffer, "hello");
-            strcat(buf, "\n");
-            strcat(buffer, buf);
-            strcat(buffer, "\n");
-            g_printf("%s\n", buffer);
-
-            // Print out the text sent to data to the label emailData
-            gtk_label_set_text(GTK_LABEL(showEmail), (const gchar *) buffer); 
-        }
-   }
-}
 
 
 int main(int argc, char **argv)
@@ -166,31 +154,12 @@ int main(int argc, char **argv)
     char i;
     scanf("%c", &i);
     if (i==y){
-        pid = fork();
-        if (pid == 0){
-            chat_write(client);
-        }
-        else {
             GtkApplication *app;
             int status;
             app = gtk_application_new ("com.hackthedeveloper", G_APPLICATION_FLAGS_NONE);
             g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
             status = g_application_run(G_APPLICATION(app), argc, argv);
             g_object_unref (app);
-        }
-        
-        // rc = pthread_create(&thread_a, NULL, chat_read, (void *)client);
-        // if (rc){
-        //     printf("ERROR; return code from pthread_create() is %d\n", rc);
-        //     exit(-1);
-        // }
-        // rc = pthread_create(&thread_b, NULL, chat_write, (void *)client);
-        // if (rc){
-        //     printf("ERROR; return code from pthread_create() is %d\n", rc);
-        //     exit(-1);
-        //     }
-        //     pthread_exit(NULL);
-        //     }
     }
     else{ 
         char message[11]="No connect";
