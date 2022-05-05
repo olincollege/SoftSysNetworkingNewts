cb0b8535108befbc9c3267ee4c09eaac7cb11efa
VedaantKuchhal <vedaantk22@gmail.com>
2022-05-04 21:26:42 -0400

    Fix GUI header file

--- GUI_server.h
+++ GUI_server.h
@@ -5,6 +5,9 @@
 #include <bluetooth/rfcomm.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
-
-#include <pthread.h>
 #include <stdio.h>
+
+
+void *chat_read(GtkWidget *showChat);
+void signup_button_clicked(GtkWidget *wid,gpointer data);
+static void activate (GtkApplication* app, gpointer user_data);
